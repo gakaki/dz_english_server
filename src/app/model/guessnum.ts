f4e8f6db58ff159171ab40c20f0cf09a171e72f8
qianjun <727435849@qq.com>
2018-01-31 15:25:28 +0800

    生成红包

--- src/app/model/guessnum.ts
+++ src/app/model/guessnum.ts
@@ -1,14 +1,63 @@
-import {double, input, model, output, string} from "../../nnt/core/proto";
-import {coldouble, colstring, table} from "../../nnt/store/proto";
+import {array, double, input, integer, model, output, required, string, type} from "../../nnt/core/proto";
+import {colarray, coldouble, colinteger, colstring, coltype, table} from "../../nnt/store/proto";
+import {DateTime} from "../../nnt/core/time";
+import {UserInfo} from "./user";
+
+
 
 @model()
-@table('kv', 'guess_packs')
+@table('kv', 'packInfo',{ttl:24*60*1000})
 export class PackInfo {
     @string(1, [output], "红包id")
     @colstring()
     pid: string;
 
-    @double(2, [input], "钱数")
+    @double(2, [input,output], "钱数")
     @coldouble()
     money: number;
+
+    @string(3, [input,output], "sid")
+    @colstring()
+    sid:string;
+
+    @type(4, UserInfo, [output], "用户信息")
+    userInfo:UserInfo;
+
+    @type(6,DateTime,[output],"创建时间")
+    @coltype(DateTime)
+    createTime=DateTime.Now;
+
+    @type(5,Set,[output],"数字密码")
+    @colarray(Set)
+    password=new Set();
+
+    @integer(7,[output],"剩余竞猜次数")
+    @colinteger()
+    guessCount=20
+}
+
+@model()
+export class Guess{
+    
+    guessNum=new Set()
+}
+
+
+@model()
+@table('kv','packGuess_records')
+export class PackGuessRecord{
+    @string(1,[output],"红包竞猜记录id")
+    @colstring()
+    pid:string;
+
+    @type(2, UserInfo, [input], "用户信息")
+    userInfo:UserInfo;
+
+    @string(3,[output],"红包竞猜数字")
+    @colstring()
+    numberWord:string;
+
+    @double(4,[output],"获取的钱数")
+    @coldouble()
+    money:number;
 }
--- src/app/router/guessnum.ts
+++ src/app/router/guessnum.ts
@@ -1,16 +1,70 @@
-import {action, IRouter} from "../../nnt/core/router";
+import {action, debug, develop, frqctl, IRouter} from "../../nnt/core/router";
 import {PackInfo} from "../model/guessnum";
 import {Trans} from "../../contrib/manager/trans";
+import {UserInfo} from "../model/user";
+import {User} from "./user";
+import {Code} from "../model/code";
+import {Random} from "../../nnt/core/kernel";
+import {Insert} from "../../nnt/manager/dbmss";
+import {DateTime} from "../../nnt/core/time";
+import {colarray, coldouble, colinteger, colstring, coltype} from "../../nnt/store/proto";
+import {double, integer, string, type} from "../../nnt/core/proto";
+import uuid = require("uuid");
+
+
+
 
 export class Guessnum implements IRouter {
-    action = "guessnum";
+    action :string = "guessnum";
 
     @action(PackInfo)
-    async sendpk(trans: Trans) {
+    async sendpack(trans: Trans){
         let m: PackInfo = trans.model;
-
-        // xxx
+        console.log("+++++++++++++++++++++++++++++++");
+        console.log(m);
+        if(!m.sid){
+            trans.status = Code.USER_NOT_FOUND;
+            trans.submit();
+            return
+        }
+        let ui:UserInfo=await User.FindUserBySid(m.sid);
+        if(ui==null){
+            trans.status = Code.USER_NOT_FOUND;
+            trans.submit();
+            return
+        }
+       m.userInfo=ui;
+        m.password=  Guessnum.getPack();
+        let pi:PackInfo=await Guessnum.savePack(m);
         trans.submit();
+    }
+
+   /* @action()
+    async guesspack(trans:Trans){
+
+    }*/
+
+    protected static getPack(){
+        let psw=new Set();
+        return getPsw(psw);
+        function getPsw(setList:Set<string>):Set<string>{
+            if(setList.size<4){
+                let p=Random.Rangei(0,9,true);
+                setList.add(p.toString());
+                return getPsw(setList);
+            }else{
+                return setList;
+            }
+        }
+    }
+
+    protected static async savePack(pack:PackInfo):Promise<PackInfo>{
+        return await Insert(PackInfo, {
+            pid: uuid.v4(),
+            money:pack.money,
+            sid:pack.sid,
+            password:pack.password
+        });
     }
 
 }
